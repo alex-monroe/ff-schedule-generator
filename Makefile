39b6ef5420bc71201e9d8c6d21170cad4fba8e9b
Alex Monroe <a.monroe@celonis.com>
2025-06-26 09:42:36 -0700

    Adds unit tests

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
-.PHONY: install build run clean
+.PHONY: install build run clean test
 
 install:
 	pip install -r requirements.txt
 
 build:
-	python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. scheduler.proto
+	python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. scheduler.proto
 
 run: build
-	python server.py
+	python3 server.py
 
 clean:
 	rm -f scheduler_pb2.py scheduler_pb2_grpc.py
+
+test:
+	PYTHONPATH=. python3 test_schedule_generator.py
---
+++ test_schedule_generator.py
@@ -0,0 +1,40 @@
+import unittest
+from unittest.mock import Mock
+import schedule_generator
+
+class TestScheduleGenerator(unittest.TestCase):
+
+    def setUp(self):
+        # Create a mock 3D array of variables for testing
+        self.mock_variables = [[[f'var_{i}_{j}_{w}' for w in range(13)] for j in range(10)] for i in range(10)]
+        schedule_generator.weeks = range(13)
+        schedule_generator.teams = range(10)
+
+    def test_getTeamsVariablesForWeek(self):
+        # Test for team 3, week 5
+        result = schedule_generator.getTeamsVariablesForWeek(self.mock_variables, 3, 5)
+        
+        # Team 3 should be playing one other team, so we expect two variables:
+        # one for (team 3 vs opponent) and one for (opponent vs team 3)
+        # However, the function gets all variables for the team in that week.
+        expected_vars = []
+        for team2 in range(10):
+            if team2 != 3:
+                expected_vars.append(f'var_3_{team2}_5')
+                expected_vars.append(f'var_{team2}_3_5')
+        
+        self.assertCountEqual(result, expected_vars)
+
+    def test_getTeamsVariablesForAllWeeks(self):
+        # Test for team 1 vs team 8
+        result = schedule_generator.getTeamsVariablesForAllWeeks(self.mock_variables, 1, 8)
+        
+        expected_vars = []
+        for w in range(13):
+            expected_vars.append(f'var_1_8_{w}')
+            expected_vars.append(f'var_8_1_{w}')
+
+        self.assertCountEqual(result, expected_vars)
+
+if __name__ == '__main__':
+    unittest.main()
