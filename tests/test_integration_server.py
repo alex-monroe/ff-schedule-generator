454cd9468377d2f4fd26d6badaa4bf0a828be75b
Alex Monroe <alexrmonroe@gmail.com>
2025-06-27 11:50:49 -0700

    Fix Makefile tabs after merge (#12)

--- tests/test_integration_server.py
+++ tests/test_integration_server.py
@@ -1,53 +1,40 @@
-import os
-import sys
-import time
 import unittest
+from pathlib import Path
 import subprocess
-from multiprocessing import Process
 
-import grpc
+# Compile the protobuf definitions for the tests
+proto_dir = Path(__file__).resolve().parents[1] / 'src' / 'protos'
+proto_file = proto_dir / 'scheduler.proto'
+subprocess.run([
+    'python3', '-m', 'grpc_tools.protoc',
+    f'-I{proto_dir}',
+    f'--python_out={proto_dir.parent}',
+    f'--grpc_python_out={proto_dir.parent}',
+    str(proto_file)
+], check=True)
 
-# Compile protobuf definitions before importing the server
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-subprocess.check_call([
-    sys.executable,
-    '-m', 'grpc_tools.protoc',
-    '-Isrc/protos',
-    '--python_out=src',
-    '--grpc_python_out=src',
-    'src/protos/scheduler.proto'
-], cwd=ROOT_DIR)
-
-import scheduler_pb2
-import scheduler_pb2_grpc
 import server
+import scheduler_pb2 as pb2
 
+class TestSchedulerServerIntegration(unittest.TestCase):
+    def setUp(self):
+        self.servicer = server.SchedulerServicer()
 
-def _run_server():
-    server.serve()
+    def _build_request(self, num_teams):
+        request = pb2.ScheduleRequest()
+        for i in range(num_teams):
+            team = pb2.Team(name=f'Team {i}', division_id=0)
+            request.league.append(team)
+        return request
 
+    def test_generate_schedule_uses_request_team_count(self):
+        num_teams = 10
+        request = self._build_request(num_teams)
+        response = self.servicer.GenerateSchedule(request, None)
 
-class TestServerIntegration(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.proc = Process(target=_run_server)
-        cls.proc.start()
-        # give the server some time to start
-        time.sleep(1)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.proc.terminate()
-        cls.proc.join()
-
-    def test_generate_schedule_default_response(self):
-        channel = grpc.insecure_channel('localhost:50051')
-        stub = scheduler_pb2_grpc.SchedulerStub(channel)
-        response = stub.GenerateSchedule(scheduler_pb2.ScheduleRequest())
-        self.assertIsInstance(response, scheduler_pb2.ScheduleResponse)
-        self.assertEqual(len(response.matchups), 0)
-        channel.close()
-
+        self.assertEqual(len(response.matchups), 13)
+        for weekly in response.matchups:
+            self.assertEqual(len(weekly.matchups), num_teams // 2)
 
 if __name__ == '__main__':
     unittest.main()
